ca2710914c2bf34684a3008e7e215a8c55451d55
James McIlroy <james.mcilroy@mirriad.com>
2014-03-07 15:25:13 +0000

    removing unreachable repos

---
+++ services/workflow-service/src/main/java/com/openshare/service/base/rpc/impl/process/definition/WorkflowDefinitionUploadHandler.java
@@ -0,0 +1,20 @@
+package com.openshare.service.base.rpc.impl.process.definition;
+
+import org.activiti.engine.RepositoryService;
+
+import com.openshare.service.base.rpc.MethodHandler;
+import com.openshare.service.base.rpc.OpenShareResponse;
+/**
+ * A simple pin handler common to everything
+ * @author james.mcilroy
+ *
+ */
+public class WorkflowDefinitionUploadHandler extends MethodHandler {
+
+	@Override
+	public OpenShareResponse handleExecution() {
+		return null;
+		
+	}
+
+}
