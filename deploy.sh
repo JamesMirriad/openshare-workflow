a8bf94a11b50c62f900d5d97967751654f35917c
James McIlroy <james.mcilroy@mirriad.com>
2014-04-02 16:22:55 +0100

    error in deploy.sh removing the tomcat work directory

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo service tomcat7 stop
-sudo rm -rf /var/lib/tomcat7/work /var/lib/tomcat7/webapps/workflow-*
+sudo rm -rf /var/lib/tomcat7/work/* /var/lib/tomcat7/webapps/workflow-*
 sudo cp -r web/workflow-ui/target/workflow-ui-0.6.0-SNAPSHOT.war /var/lib/tomcat7/webapps/workflow-ui.war
 sudo cp -r web-services/workflow-web-service/target/workflow-web-service-0.6.0-SNAPSHOT.war /var/lib/tomcat7/webapps/workflow-web-service.war
 sudo mkdir /var/lib/tomcat7/work
