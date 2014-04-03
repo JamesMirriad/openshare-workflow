9e2340b20249497bd78ed72316d1791067e95900
James McIlroy <james.mcilroy@mirriad.com>
2014-04-03 13:21:56 +0100

    fixes to file manipulation commands

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,5 @@ sudo service tomcat7 stop
 sudo rm -rf /var/lib/tomcat7/work/* /var/lib/tomcat7/webapps/workflow-*
 sudo cp -r web/workflow-ui/target/workflow-ui-0.6.0-SNAPSHOT.war /var/lib/tomcat7/webapps/workflow-ui.war
 sudo cp -r web-services/workflow-web-service/target/workflow-web-service-0.6.0-SNAPSHOT.war /var/lib/tomcat7/webapps/workflow-web-service.war
+sudo cmod -rf 777 /var/lib/tomcat7/work
 sudo service tomcat7 start
