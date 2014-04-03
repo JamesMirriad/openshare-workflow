b9278550f363f3100cffd4fc84d83d56a2cd70db
root <root@ubuntu.(none)>
2014-04-03 15:36:40 +0000

    demo_scripts

--- transcode_scripts/openshare_make_grade.sh
+++ transcode_scripts/openshare_make_grade.sh
@@ -6,5 +6,8 @@ outfile=$2
 
 boost=1
 
-ffmpeg -i $infile -f rawvideo -pix_fmt rgb24 -s $outshape pipe:1 | colour_fade.py $outshape $boost | ffmpeg -f rawvideo -pix_fmt rgb24 -s $outshape -i pipe:0 -b 3000k -y $outfile
+# create the enclosing directory for the destination file
+/usr/local/bin/create_enclosing_dir.py $outfile
+
+ffmpeg -i $infile -f rawvideo -pix_fmt rgb24 -s $outshape pipe:1 | /usr/local/bin/colour_fade.py $outshape $boost | ffmpeg -f rawvideo -pix_fmt rgb24 -s $outshape -i pipe:0 -b 3000k -y $outfile
 
